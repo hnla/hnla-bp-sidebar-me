211f17f93b96507c972dad76543f591e8ae7357b
hnla <hugoashmore@tiscali.co.uk>
2013-05-15 14:47:40 +0100

    just github  testing

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+* this is to test tabs spacing line endings etc
+*
+*/
+?>
+<p>plain html no spacing or tabs</p>
+
+<?php ?> 
+	<?php echo ' in one tab from left'; ?>
+		<?php echo 'in one tab from the one above using block indent?'; ?>
+
+<?php
+	echo 'one tab in from left';
+		echo 'two tabs in from left auto block indent off';
+?>
